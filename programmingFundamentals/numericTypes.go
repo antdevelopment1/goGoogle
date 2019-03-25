c3ae32202549cc0aed2d1d8e49f9564bff389450
April Copes <antdevelopment1@gmail.com>
2019-03-24 23:46:40 -0400

    numeric calues

--- programmingFundamentals/numericTypes.go
+++ programmingFundamentals/numericTypes.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var x int
 var y float64
+var w int8 = 127
 
 func main() {
 	x = 42
 	y = 42.73737
 	fmt.Println(x)
 	fmt.Println(y)
+	fmt.Println(w)
 	fmt.Printf("%T\n", x)
 	fmt.Printf("%T\n", y)
+	fmt.Println(runtime.GOOS)
+	fmt.Println(runtime.GOARCH)
+
 }
